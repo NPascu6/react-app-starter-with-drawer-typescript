8faecdd28e2754fb0d8bef25baf559a0c4ebe721
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2022-11-04 20:47:23 +0100

    test

--- src/components/shared/AGGridComponent.tsx
+++ src/components/shared/AGGridComponent.tsx
@@ -6,8 +6,6 @@ import {Theme, useTheme} from '@mui/material/styles';
 import clsx from 'clsx';
 import useAgGridResizeHook from '../../hooks/useAgGridResizeHook';
 import useAgGridThemeHook from '../../hooks/useAgGridThemeHook';
-import {darken} from "@mui/material";
-import {createStyles, makeStyles} from "@mui/styles";
 
 interface AGGridProps {
     items: any[],
@@ -23,62 +21,11 @@ interface AGGridProps {
     hasMultipleProps?: boolean
 }
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-    grid: {
-        flex: 1,
-        backgroundColor: theme.palette.background.paper,
-        'height': '100%',
-        '& .ag-side-buttons': {
-            width: '20px'
-        },
-        '& .ag-paging-panel': {
-            height: '20px'
-        },
-        '& .ag-header-cell': {
-            paddingLeft: '2px',
-            paddingRight: '2px'
-        },
-        '& .ag-row .ag-cell-data-changed': {
-            backgroundColor: `${darken(theme.palette.background.paper, 0.06)} !important`,
-            color: 'green',
-            fontWeight: 800,
-            fontSize: 12
-        },
-        '& .ag-header-cell-text': {
-            color: theme.textColor,
-        },
-        '& .ag-cell-value': {
-            color: theme.textColor,
-        },
-        '& .ag-header': {
-            backgroundColor: `${theme.backgroundColor} !important`
-        },
-        '& .ag-row': {
-            backgroundColor: `${theme.backgroundColor} !important`,
-            fontSize: 11,
-            color: darken(theme.palette.primary.main, 0.7),
-            '&:hover': {
-                color: `${theme.backgroundColor} !important`,
-                backgroundColor: `${theme.palette.background.paper} !important`
-            }
-        },
-        '& .ag-root-wrapper': {
-            backgroundColor: `${theme.palette.background.paper} !important`,
-            color: `${theme.textColor} !important`,
-        },
-        '& .ag-header-cell .ag-header-cell-sortable .ag-focus-managed': {
-            paddingLeft: '0.2em', paddingRight: '0.2em'
-        }
-    }
-}));
-
-
 const AGGridComponent = (props: AGGridProps) => {
     const theme: Theme = useTheme();
     const getRowStyle = useMemo(() => ({background: theme.backgroundColor, color: theme.textColor}), [theme]);
     const gridTheme: string = useAgGridThemeHook();
     useAgGridResizeHook(props.gridApi);
-    const classes = useStyles()
     /**
      * Sets row id to each row in the ag grid
      * @param params
@@ -103,7 +50,7 @@ const AGGridComponent = (props: AGGridProps) => {
         params.api.sizeColumnsToFit();
     }, []);
 
-    return <div className={clsx(gridTheme, classes.grid)} style={{height: '20em'}}>
+    return <div className={clsx(gridTheme)} style={{height: '20em'}}>
         <AgGridReact
             rowStyle={getRowStyle}
             onFirstDataRendered={onFirstDataRendered}
